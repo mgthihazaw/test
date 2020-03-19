47d65e4716c85ca4d44cfc5264d6f656555be82d
unknown <mgthihazaw777@gmail.com>
2020-03-19 14:07:08 +0630

    add model,migration and seed for admin

---
+++ database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
